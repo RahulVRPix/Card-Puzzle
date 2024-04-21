1e9d345dba18b81545b4f48f68154601d0c2de6e
RahulVRPix <rahulvrpix@gmail.com>
2024-04-22 00:28:39 +0530

    Gameplay logic added

--- Card-Shuffle-Puzzle/Assets/Scripts/PuzzleButtonController.cs
+++ Card-Shuffle-Puzzle/Assets/Scripts/PuzzleButtonController.cs
@@ -5,14 +5,24 @@ using UnityEngine.UI;
 
 public class PuzzleButtonController : MonoBehaviour
 {
-
+    public Sprite backgroundImage;
     [SerializeField]
     private Transform puzzleField;
-
     [SerializeField]
     private GameObject buttonPrefab;
 
     public List<Button> puzzleButtons = new List<Button>();
+    public Sprite[] puzzleIcons;
+    public List<Sprite> gamePuzzleIcons = new List<Sprite>();
+
+    private bool firstClick, secondClick;
+    private int firstClickIndex, secondClickIndex;
+    private string firstClickPuzzle, secondClickPuzzle;
+
+    private int countClick;
+    private int countCorrectClick;
+    private int gameGuesses;
+
 
     private void Awake()
     {
@@ -20,7 +30,7 @@ public class PuzzleButtonController : MonoBehaviour
         {
             GameObject go = Instantiate(buttonPrefab);
             go.transform.SetParent(puzzleField, false);
-            go.name = "button " + i;
+            go.name = ""+i;
 
         }
     }
@@ -28,6 +38,9 @@ public class PuzzleButtonController : MonoBehaviour
     private void Start()
     {
         GetButtons();
+        ButtonListner();
+        AddGamePuzzles();
+        gameGuesses = puzzleButtons.Count / 2;
     }
 
     private void GetButtons()
@@ -37,6 +50,89 @@ public class PuzzleButtonController : MonoBehaviour
         for(int i=0; i<gameObject.Length; i++)
         {
             puzzleButtons.Add(gameObject[i].GetComponent<Button>());
+            puzzleButtons[i].image.sprite = backgroundImage;
         }
+    }
+
+    private void ButtonListner()
+    {
+        foreach(Button btn in puzzleButtons)
+        {
+            btn.onClick.AddListener(() => PuzzleClick());
+        }
+    }
+
+    private void AddGamePuzzles()
+    {
+        int index = 0;
+        for(int i=0; i<puzzleButtons.Count; i++)
+        {
+            if(index ==  puzzleButtons.Count/2)
+            {
+                index = 0;
+            }
+            gamePuzzleIcons.Add(puzzleIcons[index]);
+            index++;
+        }
+    }
+
+    private void PuzzleClick()
+    {
+        string name = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name;
+        Debug.Log("Button Clicked!!! " + name); ;
+
+        if(firstClick == false)
+        {
+            firstClick = true;
+
+            firstClickIndex = int.Parse(UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name);
+            puzzleButtons[firstClickIndex].image.sprite = gamePuzzleIcons[firstClickIndex];
+            firstClickPuzzle = gamePuzzleIcons[firstClickIndex].name;
+        }
+        else if(secondClick == false)
+        {
+            secondClick = true;
+
+            secondClickIndex = int.Parse(UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name);
+            puzzleButtons[secondClickIndex].image.sprite = gamePuzzleIcons[secondClickIndex];
+            secondClickPuzzle = gamePuzzleIcons[secondClickIndex].name;
+
+            countClick++;
+            StartCoroutine(CheckIfPuzzleMatch());
+        }
+    }
+
+    IEnumerator CheckIfPuzzleMatch()
+    {
+        yield return new WaitForSeconds(1);
+
+        if(firstClickPuzzle == secondClickPuzzle)
+        {
+            Debug.Log("Puzzle Matched!");
+
+            yield return new WaitForSeconds(0.5f);
+            puzzleButtons[firstClickIndex].interactable = false;
+            puzzleButtons[secondClickIndex].interactable = false;
+
+            puzzleButtons[firstClickIndex].image.color = new Color(0, 0, 0, 0);
+            puzzleButtons[secondClickIndex].image.color = new Color(0, 0, 0, 0);
+
+            countCorrectClick++;
+
+            if(countCorrectClick == gameGuesses)
+            {
+                Debug.Log("Game Finished!");
+            }
+        }
+        else
+        {
+            yield return new WaitForSeconds(0.5f);
+
+            puzzleButtons[firstClickIndex].image.sprite = backgroundImage;
+            puzzleButtons[secondClickIndex].image.sprite = backgroundImage;
+        }
+
+        yield return new WaitForSeconds(0.5f);
+        firstClick = secondClick = false;
     }
 }
